3ef229822c82c5981f8cecfb9ed392841c47878b
Zavjosh05 <118492689+Zavjosh05@users.noreply.github.com>
2023-06-01 23:24:04 -0600

    cambios que probablemente ya no se usen

--- Source/PhysicGame/Base.cpp
+++ Source/PhysicGame/Base.cpp
@@ -3,10 +3,33 @@
 
 #include "Base.h"
 
-Base::Base()
+Base::Base(FString Path, ESQLiteDatabaseOpenMode OpenMode)
 {
+	Database = new FSQLiteDatabase();
+
+	if (!Database->Open(*Path, OpenMode) || !Database->IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to open database: %s"), *Database->GetLastError());
+	}
+
+	const TCHAR* SaveQuery = TEXT("replace into perfiles (id_usr, nom_usr, pass) values ()");
+	SaveStatement.Create(*Database, SaveQuery, ESQLitePreparedStatementFlags::Persistent);
+
+	const TCHAR* LoadQuery = TEXT("select * from perfiles where id_usr = $id_usr");
+	LoadStatement.Create(*Database, LoadQuery, ESQLitePreparedStatementFlags::Persistent);
 }
 
 Base::~Base()
 {
+	SaveStatement.Destroy();
+	LoadStatement.Destroy();
+	
+	if (!Database->Close())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to close database: %s"), *Database->GetLastError());
+	}
+	else
+	{
+		delete Database;
+	}
 }
